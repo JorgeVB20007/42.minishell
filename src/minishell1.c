5a5bf829798a389b0ac454e8d226cf65f4f99fd3
eM4dri <edu.mail.delivery+github@gmail.com>
2021-11-09 18:40:25 +0100

    Refactor expanse

--- src/minishell1.c
+++ src/minishell1.c
@@ -14,6 +14,77 @@ static void	ft_signal_handler(int signal)
 	rl_redisplay();
 }
 
+/**
+ * * Replace a set of characters on a string starting at some index
+ * @param str		string to modify 
+ * @param charset	set of characters to replace
+ * @param index		starting point of replacing
+*/
+char	*ft_replace(char *str, char const *oldset, char const *newset)
+{
+	char	*out;
+	int		index;
+	int		index_needle_on_haystack;
+	size_t	len;
+	char	*strnstr;
+	size_t	old_set_len;
+
+	index = -1;
+	len = ft_strlen(str);
+	old_set_len = ft_strlen(oldset);
+	index_needle_on_haystack = ft_strnstr(str, oldset, len)  - str; 
+	out = malloc(sizeof(char) * (len - old_set_len + ft_strlen(newset) + 1));
+	while (str[++index] != '\0')
+		out[index] = str[index];	
+	index = strnstr - str;
+	index = -1;
+	while (newset[++index] != '\0')
+		out[index_needle_on_haystack + index] = newset[index];
+	free(str);
+	out[index] = '\0';
+	return (out);
+}
+
+	// if (curr_char == '$' && prv_char != '\\' && \
+	// (ft_isalnum(nxt_char) || nxt_char == '_') && (qm == '"' || !qm))
+
+static void	ft_expanse_var(char **argv, t_str **env_list)
+{
+	int		i;
+	char	*strchr_$;
+	char 	*tmp;
+	size_t	len;
+	t_str 	*aux;
+
+	i = 0;
+	while (argv[i] != 0)
+	{
+		strchr_$ = ft_strchr(argv[i], '$'); 
+		while (strchr_$ != NULL && strchr_$[-1] == '\\' && (ft_isalnum(strchr_$[1]) || strchr_$[1] == '_'))
+			strchr_$ = ft_strchr(strchr_$, '$');
+		printf("%s\n", strchr_$);
+		if (!strchr_$)
+			i++;
+		else
+		{
+			len = 1;
+			while (ft_isalnum(strchr_$[len]))
+				len++;
+			printf("2 %s\n", strchr_$);
+			tmp = ft_substr(strchr_$, 1, len);
+			printf("3 %s\n", tmp);
+			aux = ft_lst_str_get_str(env_list, tmp);
+			//printf("4 %s\n", aux->str);
+			if (aux != NULL)
+				argv[i] = ft_replace(argv[i], aux->str,strchr_$ - argv[i]);
+			else
+				argv[i] = ft_replace(argv[i], "", strchr_$ - argv[i]);
+			printf("5 %s\n", argv[i]);
+			free(tmp);
+		}
+	}
+}
+
 int	main(int argc, char **argv, char **env)
 {
 	char	*str_got;
@@ -25,7 +96,7 @@ int	main(int argc, char **argv, char **env)
 //	printf("\nhye\n");
 	env_list = NULL;
 	init_ms_env(env, &env_list);
-	ft_pwd();
+	ft_array_str_print(env);
 	signal(SIGQUIT, SIG_IGN);
 	signal(SIGINT, &ft_signal_handler);
 	while (1)
@@ -43,6 +114,7 @@ int	main(int argc, char **argv, char **env)
 		else
 		{
 			param_list = modifsplit(str_got);
+			ft_expanse_var(param_list, &env_list);
 			interpreter(param_list, &env_list);
 		}
 		free(str_got);
@@ -50,4 +122,3 @@ int	main(int argc, char **argv, char **env)
 	}
 	return (0);
 }
-
