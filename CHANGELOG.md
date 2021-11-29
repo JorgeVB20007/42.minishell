797a4cc7276db550a15984b9e4db951e988d2aad
eM4dri <edu.mail.delivery+github@gmail.com>
2021-11-29 08:59:42 +0100

    Added missing documentation on log_error functions

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,13 +5,19 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
+## 2021-11-29 - Edu
+
+### Changed
+
+- **utils/error_handler.c** Added missing documentation
+
 ## 2021-11-28 - Edu
 
 ### Added
 
 - **utils/error_handler.c** where the error handles
 - add status_error to global
-- **var_expansor.c** expands exit status of the last executed command 
+- **var_expansor.c** expands exit status of the last executed command
 
 ### Changed
 
--- src/utils/error_handler.c
+++ src/utils/error_handler.c
@@ -2,6 +2,11 @@
 
 #define MS_ERROR "Minishell: "
 
+/**
+ * * Log error into STDERR_FILENO & set catched status error
+ * @param str_error		Error to log into STDERR_FILENO
+ * @param status_error	status error to set
+*/
 void	log_error(char *str_error, int status_error)
 {
 	ft_putstr_fd(MS_ERROR, STDERR_FILENO);
@@ -9,6 +14,12 @@ void	log_error(char *str_error, int status_error)
 	g_var.last_cmd_status = status_error;
 }
 
+/**
+ * * Log error into STDERR_FILENO & set catched status error
+ * * Free malloc log error input
+ * @param malloc_str_error	Error to log into STDERR_FILENO
+ * @param status_error	status error to set
+*/
 void	log_error_free(char *malloc_str_error, int status_error)
 {
 	log_error(malloc_str_error, status_error);
