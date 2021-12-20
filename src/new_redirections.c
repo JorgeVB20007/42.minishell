2a2cfb06c6db6cc693af854bcac57f4d078a13c4
Jorge Vacarisas bofill <jvacaris@c3r6s5.42madrid.com>
2021-12-20 21:28:24 +0100

    Fixed some Norminette issues

--- src/new_redirections.c
+++ src/new_redirections.c
@@ -1,5 +1,66 @@
 #include "minishell.h"
 
+static void	redir_in(int *fdi, int a, t_red *red_node, int orig_fds[2])
+{
+	char	*no_qm;
+
+	no_qm = adv_qm_rem(red_node -> redirs[a + 1], 0);
+	if (!ft_strcmp(red_node -> redirs[a], "<"))
+	{
+		if (!access(no_qm, R_OK) && ft_is_directory(no_qm))
+		{
+			if (*fdi)
+				close(*fdi);
+			*fdi = open(no_qm, O_RDONLY, 0666);
+			dup2(*fdi, 0);
+		}
+		else
+			perror("Error:");
+	}
+	if (!ft_strcmp(red_node -> redirs[a], "<<"))
+	{
+		if (*fdi)
+			close(*fdi);
+		dup2(orig_fds[0], 0);
+		if (ft_strchr(red_node -> redirs[a + 1], '\'') || \
+		ft_strchr(red_node -> redirs[a + 1], '"'))
+			ft_heredoc_qm(fdi, red_node -> redirs[a + 1], orig_fds);
+		else
+			ft_heredoc(fdi, red_node -> redirs[a + 1], orig_fds);
+		dup2(*fdi, 0);
+	}
+	free(no_qm);
+}
+
+static void	redir_out(int *fdo, int a, t_red *red_node, char *no_qm)
+{
+	if (!ft_strcmp(red_node -> redirs[a], ">"))
+	{
+		if ((!access(no_qm, W_OK) && ft_is_directory(no_qm)) \
+		|| access(no_qm, F_OK))
+		{
+			if (*fdo)
+				close(*fdo);
+			*fdo = open(no_qm, O_CREAT | O_WRONLY | O_TRUNC, 0666);
+			dup2(*fdo, 1);
+		}
+		else
+			perror("Error:");
+	}
+	if (!ft_strcmp(red_node -> redirs[a], ">>"))
+	{
+		if (!access(no_qm, R_OK) && ft_is_directory(no_qm))
+		{
+			if (*fdo)
+				close(*fdo);
+			*fdo = open(no_qm, O_CREAT | O_WRONLY | O_APPEND, 0666);
+			dup2(*fdo, 1);
+		}
+		else
+			perror("Error:");
+	}
+}
+
 void	fd_assigner(t_red *red_node, int orig_fds[2])
 {
 	int		a;
@@ -15,84 +76,21 @@ void	fd_assigner(t_red *red_node, int orig_fds[2])
 	while (red_node -> redirs[a])
 	{
 		no_qm = adv_qm_rem(red_node -> redirs[a + 1], 0);
-		if (!ft_strcmp(red_node -> redirs[a], "<"))
-		{
-			if (!access(no_qm, R_OK) && ft_is_directory(no_qm))
-			{
-				if (fdi)
-					close(fdi);
-				fdi = open(no_qm, O_RDONLY, 0666);
-				dup2(fdi, 0);
-			}
-			else
-				perror("Error:");
-		}
-		if (!ft_strcmp(red_node -> redirs[a], ">"))
-		{
-			if ((!access(no_qm, W_OK) && ft_is_directory(no_qm)) || access(no_qm, F_OK))
-			{
-				if (fdo)
-					close(fdo);
-				fdo = open(no_qm, O_CREAT | O_WRONLY | O_TRUNC, 0666);
-				dup2(fdo, 1);
-			}
-			else
-				perror("Error:");
-		}
-		if (!ft_strcmp(red_node -> redirs[a], ">>"))
-		{
-			if (!access(no_qm, R_OK) && ft_is_directory(no_qm))
-			{
-				if (fdo)
-					close(fdo);
-				fdo = open(no_qm, O_CREAT | O_WRONLY | O_APPEND, 0666);
-				dup2(fdo, 1);
-			}
-			else
-				perror("Error:");
-		}
-		if (!ft_strcmp(red_node -> redirs[a], "<<"))
-		{
-			if (fdi)
-				close(fdi);
-			fdi = 0;
-			dup2(orig_fds[0], 0);
-			if (ft_strchr(red_node -> redirs[a + 1], '\'') || ft_strchr(red_node -> redirs[a + 1], '"'))
-				ft_heredoc_qm(&fdi, red_node -> redirs[a + 1], orig_fds);
-			else
-				ft_heredoc(&fdi, red_node -> redirs[a + 1], orig_fds);
-			dup2(fdi, 0);
-		}
+		redir_out(&fdo, a, red_node, no_qm);
+		redir_in(&fdi, a, red_node, orig_fds);
 		free(no_qm);
 		a += 2;
 	}
 }
 
-/*
-?   Aquí quería llamar a una función para rellenar la lista enlazada
-?   para luego crear forks en un while y pasarle a cada fork su item de la
-?   lista con su path, sus parámetros y todas sus redirecciones.
-*   Lo que es seguro es que cada item de la lista es un comando, y se 
-*   separan los unos de los otros al encontrarse con un | sin comillas.
-*/
-void	new_redirections(char **list, t_str **env_list)
+static void	apply2pipes(int pip[2], t_red *red_list, int items, char **env_array)
 {
-	char	**env_array;
-	int		items;
-	int		ctr;
-	int		pip[2];
-	t_red	*red_list;
-	t_red	**red_list2;
-	int		status;
-	int		orig_fds[2];
+	int	ctr;
+	int	orig_fds[2];
 
-	red_list = NULL;
+	ctr = 0;
 	orig_fds[0] = dup(0);
 	orig_fds[1] = dup(1);
-	items = put_params_in_struct(list, env_list, &red_list);
-	red_list2 = &red_list;
-	env_array = lst_str_to_array(env_list);
-	ctr = 0;
 	while (ctr++ != items)
 	{
 		if (ctr != items)
@@ -117,8 +115,28 @@ void	new_redirections(char **list, t_str **env_list)
 		dup2(orig_fds[0], 0);
 		dup2(orig_fds[1], 1);
 	}
-	ctr = 0;
-	while (ctr++ != items)
+}
+
+/*
+?   Aquí quería llamar a una función para rellenar la lista enlazada
+?   para luego crear forks en un while y pasarle a cada fork su item de la
+?   lista con su path, sus parámetros y todas sus redirecciones.
+*   Lo que es seguro es que cada item de la lista es un comando, y se 
+*   separan los unos de los otros al encontrarse con un | sin comillas.
+*/
+void	new_redirections(char **list, t_str **env_list)
+{
+	char	**env_array;
+	int		items;
+	int		pip[2];
+	t_red	*red_list;
+	int		status;
+
+	red_list = NULL;
+	items = put_params_in_struct(list, env_list, &red_list);
+	env_array = lst_str_to_array(env_list);
+	apply2pipes(pip, red_list, items, env_array);
+	while (items--)
 	{
 		wait(&status);
 		g_var.last_cmd_status = WEXITSTATUS(status);
